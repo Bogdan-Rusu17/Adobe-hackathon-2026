84c54f004c9b69592cf5bb977a499f199f5dc72a
Bogdan-Rusu17 <rusub2003@gmail.com>
2025-11-16 13:29:51 +0200

    make knexfile be javascript

---
+++ backend/knexfile.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+
+module.exports = {
+    development: {
+        client: "pg",
+        connection: process.env.DATABASE_URL,
+        migrations: {
+            directory: "./src/migrations",
+        },
+    },
+    production: {
+        client: "pg",
+        connection: process.env.DATABASE_URL,
+        migrations: {
+            directory: "./src/migrations",
+        },
+    },
+};
+
