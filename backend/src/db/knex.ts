01cc37269a7df334236e3d302f81fc1256a789e9
Bogdan-Rusu17 <rusub2003@gmail.com>
2025-11-16 11:30:07 +0200

    add separate config for db object

--- backend/src/db/knex.ts
+++ backend/src/db/knex.ts
@@ -1,4 +1,14 @@
 import knex from "knex";
-import config from "../../knexfile";
 
-export default knex(config.development);
+export const dbConfig = {
+    client: "pg",
+    connection: {
+        host: process.env.DB_HOST,
+        port: Number(process.env.DB_PORT),
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+    }
+};
+
+export default knex(dbConfig);
