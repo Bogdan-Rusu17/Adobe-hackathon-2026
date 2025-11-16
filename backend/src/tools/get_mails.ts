c21cdf3061e17a680982ee6bacb10acda637b5c4
Petru-Vlad Ionescu <ionescupv@gmail.com>
2025-11-16 14:38:34 +0200

    Add get_mails tool

---
+++ backend/src/tools/get_mails.ts
@@ -0,0 +1,38 @@
+import { google } from "googleapis";
+import { tool, ToolRuntime } from "langchain";
+import { contextSchema } from "../agent";
+import * as z from "zod";
+
+
+const TOOL_DESCRIPTION = `
+Get recent mails from the user's inbox.
+`
+
+export const getMails = tool(async (_, runtime: ToolRuntime<any, typeof contextSchema>) => {
+	const auth = new google.auth.OAuth2();
+	auth.setCredentials({ access_token: runtime.context.accessToken });
+	const gmail = google.gmail({ version: 'v1', auth });
+
+	const res = await gmail.users.messages.list({
+		userId: 'me',
+		maxResults: 10,
+	});
+
+	const messages = res.data.messages || [];
+	let messagesSnippets = [];
+	for (const message of messages) {
+		const msg = await gmail.users.messages.get({
+			userId: 'me',
+			id: message.id!,
+		});
+		messagesSnippets.push(msg.data.snippet);
+	}
+
+	return messagesSnippets;
+},
+{
+	name: "get_mails",
+	description: TOOL_DESCRIPTION,
+	schema: z.undefined(),
+}
+);
